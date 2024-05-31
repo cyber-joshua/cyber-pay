f53183bebffd5f5675b0f95030b11b33c5c63d56
cyber-joshua <joshua@cybertinolab.com>
2024-05-31 16:41:33 +0800

    added shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ScanButton from "@/components/ScanButton";
 
 export default function Home() {
   return (
-    <main className="flex h-screen flex-col items-center justify-center">
+    <main className="flex h-screen flex-col items-center justify-between">
       <ScanButton />
     </main>
   );
