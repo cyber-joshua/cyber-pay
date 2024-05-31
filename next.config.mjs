eb33a90d418a4605345d9c7d3e472a3a45633e73
cyber-joshua <joshua@cybertinolab.com>
2024-05-31 15:03:56 +0800

    added next-pwa and html5-qrcode

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 
-export default nextConfig;
+export default withPWA(nextConfig);
